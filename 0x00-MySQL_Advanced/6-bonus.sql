b5f099f660d9207d1a72240c55b6f66ff315c2da
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-19 02:43:49 +0200

    6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,17 @@
 -- stored procedure add bonus
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE add_bonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-    DECLARE project_id INT;
-    IF NOT EXISTS (SELECT * FROM projects WHERE name = project_name) THEN
+    DECLARE project_id INT DEFAULT 0;
+    
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    
+    IF project_id = 0 THEN
         INSERT INTO projects (name) VALUES (project_name);
         SET project_id = LAST_INSERT_ID();
     END IF;
+    
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END $$
 DELIMITER ;
