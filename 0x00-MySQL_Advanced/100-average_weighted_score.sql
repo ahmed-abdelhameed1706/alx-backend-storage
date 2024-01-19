c79874c967b05ecb73f4cbce71d9f57bad3f20f2
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-19 03:41:17 +0200

    100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,9 +6,12 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
-    SET average_score = ( SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-                          FROM corrections, projects
-                          WHERE corrections.project_id = projects.id
-                          AND corrections.user_id = user_id
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections, projects
+        WHERE corrections.project_id = projects.id
+        AND corrections.user_id = user_id
                         )
     WHERE id = user_id;
+END $$
+DELIMITER ;
