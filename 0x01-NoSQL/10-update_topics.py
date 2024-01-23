eba34d2b4d1128a381b3efa668e8e4f8744f30e6
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-22 23:05:38 -0800

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function to change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function to update topics"""
+    mongo_collection.update_one({"name": name},{"$set":{"topics": topics}})
