8ff2cbd48438a000d6c8e97eeb12bde4691afb01
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-23 00:45:08 -0800

    102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" script to get some stats from logs"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """script to get some stats from logs"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs.nginx
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{db.count_documents({})} logs")
+
+    print("Methods:")
+
+    for method in methods:
+        print(f"\tmethod {method}: {db.count_documents({'method': method})}")
+
+    query = {'method': 'GET', 'path': '/status'}
+
+    print(f"{db.count_documents(query)} status check")
+
+    print("IPS:")
+
+    pipeline = [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10}
+            ]
+
+    ips = db.aggregate(pipeline)
+
+    for ip in ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
