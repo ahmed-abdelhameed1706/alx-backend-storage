00009b0132b5ac35b53567185af793d8f82972c3
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-24 12:03:54 +0200

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """cache class"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> None:
+        """function to get back data from redis"""
+        data = self._redis.get(key)
+        if not data:
+            return
+
+        if fn:
+            formatted_data = fn(data)
+            return formatted_data
+        if fn is int:
+            return self.get_int(data)
+        if fn in str:
+            return self.get_str(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        return data.decode("utf-8")
+
+    def get_int(self, data: bytes) -> int:
+        return int(data)
